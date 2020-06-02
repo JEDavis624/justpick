cbd51c83e1f702d13dce035817466cfa2f28a6ee
John Davis <johnedavis624@gmail.com>
2020-06-02 18:16:14 -0500

    about us

--- client/src/containers/AboutUs.js
+++ client/src/containers/AboutUs.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react';
-import MainJumbotron from '../components/Jumbotron';
-import { Row, Col } from 'react-bootstrap';
+// import React, { Component } from 'react';
+// import MainJumbotron from '../components/Jumbotron';
+// import { Row, Col } from 'react-bootstrap';
 
-class AboutUs extends Component {
+// class AboutUs extends Component {
 
-    render() {
-        return(
-            <div>
-                <MainJumbotron />
+//     render() {
+//         return(
+//             <div>
+//                 <MainJumbotron />
 
-                <Row>
-                    <h2 className="aboutUsHeaders">About Us</h2>
-                </Row>
+//                 <Row>
+//                     <h2 className="aboutUsHeaders">About Us</h2>
+//                 </Row>
 
-                <Row>
-                    <Col md={1}></Col>
-                    <Col md={10}>
-                        <p></p>
-                    </Col>
-                        <Col md={1}></Col>
-                </Row>
+//                 <Row>
+//                     <Col md={1}></Col>
+//                     <Col md={10}>
+//                         <p></p>
+//                     </Col>
+//                         <Col md={1}></Col>
+//                 </Row>
 
-                <Row>
-                    <h2 className="aboutUsHeaders">Meet the team</h2>
-                </Row>
+//                 <Row>
+//                     <h2 className="aboutUsHeaders">Meet the team</h2>
+//                 </Row>
 
-                <Row>
+//                 <Row>
                     
-                    <Col md={4}>
-                        <div>
-                            <h3 className="aboutUsHeaders"> Jariel Brown </h3>
-                            <img id="avatar" src="../../public/images/jariel.jpg" alt="Jariel Brown"></img>
-                            <p id="autobio"></p>
-                        </div>
-                    </Col>
-                    <Col md={4}>
-                        <h3 className="aboutUsHeaders"> Mesut Sipar </h3>
-                        <img id="avatar" src="../../public/images/mesut.png" alt="Mesut Sipar"></img>
-                        <p id="autobio"></p>
-                    </Col>
-                    <Col md={4}>
-                        <h3 className="aboutUsHeaders"> John Davis </h3>
-                        <img id="avatar" src="../../public/images/john.jpg" alt="John Davis"></img>
-                        <p id="autobio"></p>
-                    </Col>
+//                     <Col md={4}>
+//                         <div>
+//                             <h3 className="aboutUsHeaders"> Jariel Brown </h3>
+//                             <img id="avatar" src="../../public/images/jariel.jpg" alt="Jariel Brown"></img>
+//                             <p id="autobio"></p>
+//                         </div>
+//                     </Col>
+//                     <Col md={4}>
+//                         <h3 className="aboutUsHeaders"> Mesut Sipar </h3>
+//                         <img id="avatar" src="../../public/images/mesut.png" alt="Mesut Sipar"></img>
+//                         <p id="autobio"></p>
+//                     </Col>
+//                     <Col md={4}>
+//                         <h3 className="aboutUsHeaders"> John Davis </h3>
+//                         <img id="avatar" src="../../public/images/john.jpg" alt="John Davis"></img>
+//                         <p id="autobio"></p>
+//                     </Col>
             
-                </Row>
-            </div>
-        )
+//                 </Row>
+//             </div>
+//         )
 
-    }
-}
+//     }
+// }
 
-export default AboutUs;
+// export default AboutUs;
